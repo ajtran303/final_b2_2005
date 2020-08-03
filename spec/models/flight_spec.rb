264774922975eb47fc05a936b778e9eea04ec272
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-08-03 11:38:07 -0600

    Add spec for Flight model methods count_minor_passengers and count_adult_passengers

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -21,4 +21,35 @@ RSpec.describe Flight do
       expect(Flight.get_flight_id_from_flight_number(flight.number)).to eq(flight.id)
     end
   end
+
+  describe "instance methods" do
+    before :each do
+      frontier = Airline.create(name: "Frontier")
+
+      @flight = Flight.create(number: "1727",
+                             date: "08/03/20",
+                             time: "8:00 AM",
+                             departure_city: "Denver",
+                             arrival_city: "Reno",
+                             airline_id: frontier.id)
+
+      joe = Passenger.create(name: "Joe", age: 7)
+      jackie = Passenger.create(name: "Jackie", age: 17)
+      jenny = Passenger.create(name: "Jackie", age: 18)
+      jill = Passenger.create(name: "Jill", age: 21)
+      jamie = Passenger.create(name: "Jamie", age: 28)
+
+      passengers = [joe, jackie, jenny, jill, jamie]
+
+      @flight.passengers << passengers
+    end
+
+    it "#count_minor_passengers" do
+      expect(@flight.count_minor_passengers).to eq(2)
+    end
+
+    it "#count_adult_passengers" do
+      expect(@flight.count_adult_passengers).to eq(3)
+    end
+  end
 end
