b894483c1d9e53e44ee9d1d55c7bbd76384b8d1d
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-08-03 10:16:25 -0600

    Add feature: passengers show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "/flights/:id", to: "flights#show"
+  get "/flights/:id", to: "flights#show", as: "flights"
+  get "/passengers/:id", to: "passengers#show", as: "passengers"
 end
