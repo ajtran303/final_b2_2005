74613a80f84d1b8dc187cd14b3b2e49db5244e31
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-08-03 10:55:01 -0600

    Add spec for Flight.get_flight_id_from_flight_number

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Flight do
     it { should have_many :flight_passengers }
     it { should have_many(:passengers).through(:flight_passengers) }
   end
+
+  describe "class methods" do
+    it ".get_flight_id_from_flight_number" do
+      airline = Airline.create(name: "Frontier")
+
+      flight = Flight.create(number: "1727",
+                             date: "08/03/20",
+                             time: "8:00 AM",
+                             departure_city: "Denver",
+                             arrival_city: "Reno",
+                             airline_id: airline.id)
+
+      expect(Flight.get_flight_id_from_flight_number(flight.number)).to eq(flight.id)
+    end
+  end
 end
