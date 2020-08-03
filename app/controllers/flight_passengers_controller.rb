28e3c7a911a8f59808de840b127008b0eb49dfcd
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-08-03 11:25:41 -0600

    Add feature: passengers can only be added to a flight once

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,9 +1,26 @@
 class FlightPassengersController < ApplicationController
   def create
+    if can_add_passenger?(passenger.id, flight.id)
+      FlightPassenger.create(passenger_id: passenger.id, flight_id: flight.id)
+    else
+      flash[:notice] = "#{passenger.name} is already on Flight #{flight.number}."
+    end
+    redirect_to passengers_path(passenger)
+  end
+
+  private
+
+  def can_add_passenger?(passenger_id, flight_id)
+    flight_passenger = FlightPassenger.where(passenger_id: passenger_id, flight_id: flight_id)
+    flight_passenger.empty?
+  end
+
+  def passenger
+    @passenger ||= Passenger.find(params[:passenger_id])
+  end
+
+  def flight
     flight_id = Flight.get_flight_id_from_flight_number(params[:flight_number])
-    passenger_id = params[:passenger_id]
-    
-    FlightPassenger.create(passenger_id: passenger_id, flight_id: flight_id)
-    redirect_to passengers_path(passenger_id)
+    @flight ||= Flight.find(flight_id)
   end
 end
