bb5ee3c57726301ef20354a4a2ba21dcb44a6e8a
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-08-03 09:46:14 -0600

    Add time column to flights table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200803152104) do
+ActiveRecord::Schema.define(version: 20200803154449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20200803152104) do
     t.bigint "airline_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "time"
     t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
