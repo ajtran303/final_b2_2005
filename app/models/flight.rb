88237c6559f79eb6bd2dc028463b45ecf4d4b8bd
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-08-03 10:55:14 -0600

    Add Flight.get_flight_id_from_flight_number

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def self.get_flight_id_from_flight_number(flight_number)
+    self.where(number: flight_number).distinct.first.id
+  end
 end
